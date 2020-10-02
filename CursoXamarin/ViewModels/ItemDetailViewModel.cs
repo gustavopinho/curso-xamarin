d88737a47e5fe3bade72408767c6d9f831c9c325
user182276 <user182276@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-10-02 14:18:01 -0400

    Iniciando projeto

---
+++ CursoXamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CursoXamarin.Models;
+
+namespace CursoXamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
